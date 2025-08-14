883cfbf7bbffaa47e940b5f3c5b6781b9c8d4b5a
ELF32bit <101274435+ELF32bit@users.noreply.github.com>
2025-08-14 08:47:34 +0300

    Update trigger_hurt.gd

--- mapping/quake/builders/trigger_hurt.gd
+++ mapping/quake/builders/trigger_hurt.gd
@@ -17,6 +17,6 @@ static func build(map: MapperMap, entity: MapperEntity) -> Node:
 
 	# binding trigger_hurt properties
 	bind_targetname_base(entity)
-	entity.set("damage", entity.get_int_property("damage", 5))
+	node.set("damage", entity.get_int_property("damage", 5))
 
 	return node
