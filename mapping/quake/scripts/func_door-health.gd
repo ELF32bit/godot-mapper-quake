5201705dcf0dd9413bb348a4922f7ffb58fa4674
ELF32bit <101274435+ELF32bit@users.noreply.github.com>
2025-10-29 01:48:08 +0300

    Fixed func_door health emitting wrong signal

--- mapping/quake/scripts/func_door-health.gd
+++ mapping/quake/scripts/func_door-health.gd
@@ -11,7 +11,7 @@ signal activated # emitted when health ended or generic signal received by linke
 		var previous_health := int(quake_health)
 		quake_health = clampi(value, 0, max_health)
 		if quake_health == 0 and quake_health != previous_health:
-			generic.emit()
+			activated.emit()
 
 
 func _on_opening_signal() -> void:
